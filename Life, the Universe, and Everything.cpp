5abe49e7946920a39647a812291c0148f44c48f2
N V P Siva Priyatam <89910151+nsiva28@users.noreply.github.com>
2021-12-26 17:47:42 +0530

    Solution

---
+++ Life, the Universe, and Everything.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void test_case() {
+    int n;
+    while (cin >> n) {
+        if (n == 42)break;
+        cout << n << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+//    cin >> t;
+    while (t--) {
+        test_case();
+    }
+    return 0;
+}
